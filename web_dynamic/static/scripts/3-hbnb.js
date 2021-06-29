1d826925c85410ca83fbbd4ac6fa8cde2866e636
szbrooks2017 <strattonbrooks13@gmail.com>
2021-06-29 22:29:28 +0000

    fixing text for place

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 
 $(function () {
   $.getJSON(
-    'http://4ad65c508361.7e5652f1.hbtn-cod.io:5001/api/v1/status/',
+    'http://2a7794c2eec7.7e5652f1.hbtn-cod.io:5001/api/v1/status/',
     function (data) {
       if (data.status === 'OK') {
         $('DIV#api_status').addClass('available');
@@ -30,9 +30,8 @@ $(function () {
 
 $(function () {
   $.ajax({
-    url: 'http://4ad65c508361.7e5652f1.hbtn-cod.io:5001/api/v1/places_search/',
+    url: 'http://2a7794c2eec7.7e5652f1.hbtn-cod.io:5001/api/v1/places_search/',
     method: 'POST',
-    dataType: 'json',
     contentType: 'application/json',
     data: JSON.stringify({}),
     success: function (data) {
@@ -42,13 +41,13 @@ $(function () {
           '<article>' +
           '<div class="title_box"><h2>' + place.name + '</h2>' +
           '<div class="price_by_night">$' + place.price_by_night + '</div></div>' +
-          '<div class="information"><div class="max_guest">' + place.max_guest + 'Guest' +
-          (place.max_guest != 1 ? 's</div>' : '</div>') +
-          '<div class="number_rooms">' + place.number_rooms + ' Bedroom' +
-          (place.number_rooms != 1 ? 's</div>' : '</div') +
-          '<div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' +
-          (place.number_bathrooms != 1 ? 's</div>' : '</div>') +
-          '<div class="description">' + place.description + '</div></article>');
+          '<div class="information"><div class="max_guest">' + place.max_guest +
+          (place.max_guest != 1 ? 'Guests </div>' : 'Guest </div>') +
+          '<div class="number_rooms">' + place.number_rooms +
+          (place.number_rooms != 1 ? 'Bedrooms </div>' : 'Bedroom </div') +
+          '<div class="number_bathrooms">' + place.number_bathrooms +
+          (place.number_bathrooms != 1 ? 'Bathrooms </div>' : 'Bathroom </div>') +
+          '</div><div class="description">' + place.description + '</div></article>');
       }
     }
   });
